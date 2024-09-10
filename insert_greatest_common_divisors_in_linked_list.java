d10e228b2ce090b7afd0d17dcc14a5e33aaf81a8
Anshu Rai <anshu.rai018@gmail.com>
2024-09-10 20:12:42 +0530

    day 08

---
+++ insert_greatest_common_divisors_in_linked_list.java
@@ -0,0 +1,21 @@
+class Solution {
+    public ListNode insertGreatestCommonDivisors(ListNode head) {
+        ListNode prev = head, curr = prev.next;
+        while(curr != null){
+           ListNode newNode = new ListNode(gcdEuclid(prev.val, curr.val));
+           prev.next = newNode;
+           newNode.next = curr;
+           prev = curr;
+           curr = curr.next;
+        }
+        return head;
+    }
+    private int gcdEuclid(int a, int b){
+        while(b != 0){
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
